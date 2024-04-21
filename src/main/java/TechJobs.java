73810c62e2c57dd6801904b70629e4c0548668d0
chrisbusche26 <chrisbusche26@gmail.com>
2024-04-20 22:07:20 -0500

    Unit 2 GA1 complete.  All local tests passed

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.print("No Results");
+        } else {
+            for (HashMap<String, String> map : someJobs) {
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    System.out.println(key + ": " + value);
+                }
+                System.out.println("*****");
+            }
+        }
     }
 }
+
